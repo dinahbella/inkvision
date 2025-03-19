7d5e2b056b38b41b52a50e8764da54118392eeb5
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-19 12:10:05 +0100

    global

--- Backend/components/LoginLayout.js
+++ Backend/components/LoginLayout.js
@@ -2,23 +2,20 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
 export default function LoginLayout({ children }) {
-  const { data: session, status } = useSession(); // Get session and status
-  const router = useRouter();
-
-  if (status === "loading") {
-    return (
-      <div className="full-h flex flex-center">
-        <div className="loading-bar">Loading...</div>
-      </div>
-    );
-  }
-
-  if (!session) {
-    // Redirect to sign-in page if the user is not authenticated
-    router.push("/api/auth/signin");
-    return null; // Return null to prevent rendering anything else
-  }
-
-  // If the user is authenticated, render the children
+  //   const { data: session, status } = useSession(); // Get session and status
+  //   const router = useRouter();
+  //   if (status === "loading") {
+  //     return (
+  //       <div className="full-h flex flex-center">
+  //         <div className="loading-bar">Loading...</div>
+  //       </div>
+  //     );
+  //   }
+  //   if (!session) {
+  //     // Redirect to sign-in page if the user is not authenticated
+  //     router.push("/api/auth/signin");
+  //     return null; // Return null to prevent rendering anything else
+  //   }
+  //   // If the user is authenticated, render the children
   return <>{children}</>;
 }
--- Backend/pages/auth/signup.js
+++ Backend/pages/auth/signup.js
@@ -1,11 +1,26 @@
-import React from "react";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Signup() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
   return (
     <>
       <div className="flex flex-center full-h">
         <div className="loginform">
-          <div className="heading">Sign Up</div>
+          <div className="heading">Sign Up Create Admin</div>
+          <form>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button type="submit">Sign Up</button>
+          </form>
         </div>
       </div>
     </>
