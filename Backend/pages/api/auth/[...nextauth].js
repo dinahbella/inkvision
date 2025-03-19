66ada7a9d41c76dc45a7dda22f975abfca6f4b5e
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-19 10:36:07 +0100

    auth

--- Backend/pages/api/auth/[...nextauth].js
+++ Backend/pages/api/auth/[...nextauth].js
@@ -1,5 +1,21 @@
-import React from "react";
+import { signIn } from "next-auth/react";
 
-export default function auth() {
-  return <div>auth</div>;
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, password } = req.body;
+
+    const result = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+
+    if (result.error) {
+      res.status(401).json({ error: result.error });
+    } else {
+      res.status(200).json({ message: "Sign-in successful" });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
 }
